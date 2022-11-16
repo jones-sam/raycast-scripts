8c1d8df7491bad6f079da3b2efe5278b8a5e514b
Sam Jones <45644425+jones-sam@users.noreply.github.com>
2022-11-15 22:16:19 -0400

    start time machine backup

---
+++ start-time-machine-backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Start Time Machine Backup
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon ⏳
+
+# Documentation:
+# @raycast.author Sam Jones
+# @raycast.authorURL https://github.com/jones-sam
+
+tmutil startbackup
