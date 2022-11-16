caf0976fcf27ade315fb48a38f5d4d11c791b5c1
Sam Jones <45644425+jones-sam@users.noreply.github.com>
2022-11-15 22:01:22 -0400

    eject time machine

---
+++ eject-time-machine.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Eject Time Machine
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon ⏱️
+
+# Documentation:
+# @raycast.author Sam Jones
+# @raycast.authorURL https://github.com/jones-sam
+
+volume="APFS Volume Extreme SSD"
+if diskutil list external virtual | ack "$volume" ; then
+    diskutil unmount "Extreme SSD"
+else
+    echo "Time Machine drive not mounted" ; exit 1
+fi
